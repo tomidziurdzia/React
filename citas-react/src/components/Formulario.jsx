399d83002de73a150437bf5510f69d2e49de7467
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-16 15:59:47 -0300

    useState y onChange en Formulario

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,15 +1,18 @@
 import {useState, useEffect} from 'react'
 
 const Formulario = () => {
+
+  const [nombre, setNombre] = useState('')
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-      <p className="text-lg mt-5 text-center mb-10">
+      <p className="text-lg mt-5 text-center">
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mt-5">
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 font-bold">
             Nombre Mascota
@@ -19,6 +22,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre de la mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
         <div className="mb-5">
--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -4,7 +4,7 @@ const ListadoPacientes = () => {
   return (
     <div className="md:w-1/2 lg:w-3/5">
       <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
+      <p className="text-xl mt-5 text-center">
         Administra tus {''}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
