1d0d297fcbdeb23a6038cbd0da20a8ec6d83d430
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-16 08:55:32 -0300

    Formulario Pacientes Terminado

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -2,10 +2,71 @@ const Formulario = () => {
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-      <p className="text-lg mt-5 text-center">
+      <p className="text-lg mt-5 text-center mb-10">
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div className="mb-5">
+          <label htmlFor="mascota" className="block text-gray-700 font-bold">
+            Nombre Mascota
+          </label>
+          <input 
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="propietario" className="block text-gray-700 font-bold">
+            Nombre Propietario
+          </label>
+          <input 
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="email" className="block text-gray-700 font-bold">
+            Email
+          </label>
+          <input 
+            id="email"
+            type="email"
+            placeholder="Email del propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="alta" className="block text-gray-700 font-bold">
+            Alta
+          </label>
+          <input 
+            id="alta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="sintomas" className="block text-gray-700 font-bold">
+            Sintomas
+          </label>
+          <textarea
+            id="sintomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Describe los sintomas"
+          />
+        </div>
+        <input 
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white font-bold hover:bg-indigo-700 cursor-pointer transition-all rounded-md"
+          value="Agregar Paciente"
+        />
+      </form>
     </div>
   )
 }
