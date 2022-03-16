f76077efe62abbdfca5ff05c18083b6bca359896
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-16 19:15:57 -0300

    Eliminar paciente

--- citas-react/src/components/Paciente.jsx
+++ citas-react/src/components/Paciente.jsx
@@ -1,6 +1,14 @@
-const Paciente = ({paciente, setPaciente}) => {
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
 
-  const {nombre, propietario, email, fecha, sintomas} = paciente
+  const {nombre, propietario, email, fecha, sintomas, id} = paciente
+
+  const handleEliminar = () => {
+    const respuesta = confirm('Deseas eliminar este paciente?')
+
+    if(respuesta) {
+      eliminarPaciente(id)
+    }
+  }
 
   return(
     <div className="m-5 bg-white shadow-md px-5 py-10 rounded-xl">
@@ -31,6 +39,7 @@ const Paciente = ({paciente, setPaciente}) => {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 cursor-pointer rounded-lg text-white font-bold"
+          onClick={handleEliminar}
         >
           Eliminar
         </button>
