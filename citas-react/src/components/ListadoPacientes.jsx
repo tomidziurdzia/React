34b7972fa931ba8ed42166da24aa1b7028844f2b
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-16 09:16:04 -0300

    Listado Pacientes terminado y terminado todos los estilos

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -2,16 +2,18 @@ import Paciente from "./Paciente"
 
 const ListadoPacientes = () => {
   return (
-    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+    <div className="md:w-1/2 lg:w-3/5">
       <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
       <p className="text-xl mt-5 mb-10 text-center">
         Administra tus {''}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
+      <div className="md:h-screen overflow-y-scroll">
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+      </div>
     </div>
   )
 }
