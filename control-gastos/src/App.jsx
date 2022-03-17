0d13f0b8a8c02a9d514be14c2d61b99cc9cac646
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-17 13:22:34 -0300

    Formatear numero a moneda

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 
 function App() {
 
-  const [presupuesto, setPresupuesto] = useState(0)
+  const [presupuesto, setPresupuesto] = useState('')
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
   return (
