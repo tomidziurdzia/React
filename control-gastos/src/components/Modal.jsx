6c3f66a427de161616425e2a9cb28d45f21ee6ba
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-17 14:29:56 -0300

    Formulario de gastos terminado

--- control-gastos/src/components/Modal.jsx
+++ control-gastos/src/components/Modal.jsx
@@ -21,6 +21,43 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
       </div>
       <form className={`formulario ${animarModal ? 'animar' : ''}`}>
         <legend>Nuevo Gasto</legend>
+        <div className='campo'>
+          <label htmlFor='nombre'>Nombre Gasto</label>
+          <input
+           id="nombre"
+            type="text"
+            placeholder='Añade el Gombre del Gato'
+          />
+        </div>
+        <div className='campo'>
+          <label htmlFor='cantidad'>Cantidad</label>
+          <input
+           id="cantidad"
+            type="number"
+            placeholder='Añade la cantidad del gasto'
+          />
+        </div>
+        <div className='campo'>
+          <label htmlFor='categoria'>Categoria</label>
+          <select
+            id='categoria'
+          >
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+        
+        <input 
+          type='submit'
+          value='Añadir Gasto'
+        />
+
       </form>
     </div>
   )
