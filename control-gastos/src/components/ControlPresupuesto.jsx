757502e0f26aaaafafa6293a5d1dc874938b9463
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-17 15:53:16 -0300

    Calculando presupuesto gastado

--- control-gastos/src/components/ControlPresupuesto.jsx
+++ control-gastos/src/components/ControlPresupuesto.jsx
@@ -1,4 +1,16 @@
-const ControlPresupuesto = ({presupuesto}) => {
+import { useState, useEffect } from "react";
+
+const ControlPresupuesto = ({gastos, presupuesto}) => {
+
+  const [disponible, setDisponible] = useState(0)
+  const [gastado, setGastado] = useState(0)
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
+
+    setGastado(totalGastado)
+  },[gastos])
+
 
   const formatearCantidad = (cantidad) => {
     return cantidad.toLocaleString('es-ar', {
@@ -17,10 +29,10 @@ const ControlPresupuesto = ({presupuesto}) => {
           <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
         </p>
         <p>
-          <span>Disponible:</span> {formatearCantidad(0)}
+          <span>Disponible:</span> {formatearCantidad(disponible)}
         </p>
         <p>
-          <span>Gastado:</span> {formatearCantidad(0)}
+          <span>Gastado:</span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
