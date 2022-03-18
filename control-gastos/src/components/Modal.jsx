64641235012a945c779c7fb7fa333c811b55dce8
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-18 09:07:17 -0300

    editando gastos

--- control-gastos/src/components/Modal.jsx
+++ control-gastos/src/components/Modal.jsx
@@ -8,12 +8,16 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar
     const [nombre, setNombre] = useState('')
     const [cantidad, setCantidad] = useState('')
     const [categoria, setCategoria] = useState('')
+    const [fecha, setFecha] = useState('')
+    const [id, setId] = useState('')
 
     useEffect(() => {
       if(Object.keys(gastoEditar).length > 0) {
         setNombre(gastoEditar.nombre)
         setCantidad(gastoEditar.cantidad)
         setCategoria(gastoEditar.categoria)
+        setId(gastoEditar.id)
+        setFecha(gastoEditar.fecha)
       }
     }, [])
 
@@ -37,7 +41,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar
       return
     }
 
-    guardarGasto({nombre, cantidad, categoria})
+    guardarGasto({nombre, cantidad, categoria, id, fecha})
   }
 
   return (
