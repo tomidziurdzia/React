4f4087473a5b7a867f485b87da5d87c1ff1bdb59
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-22 20:49:26 -0300

    Consultando la API

--- cripto-cot/src/components/Formulario.jsx
+++ cripto-cot/src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
@@ -22,6 +22,17 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      console.log(resultado.Data);
+    };
+    consultarAPI();
+  }, []);
 
   return (
     <form>
