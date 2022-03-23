d5f15e9ef2cbd4b9c1eac103702ba81de9d738b8
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-23 09:20:53 -0300

    Finalizacion

--- cripto-cot/src/App.jsx
+++ cripto-cot/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import Formulario from "./components/Formulario";
 import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
 import ImagenCripto from "./img/imagen-criptos.png";
 
 const Contenedor = styled.div`
@@ -43,10 +44,13 @@ const Heading = styled.h1`
 function App() {
   const [monedas, setMonedas] = useState({});
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if (Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
         const {moneda, criptomoneda} = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
 
@@ -54,6 +58,7 @@ function App() {
         const resultado = await respuesta.json()
 
         setResultado(resultado.DISPLAY[criptomoneda][moneda])
+        setCargando(false)
         
       }
       cotizarCripto()
@@ -66,6 +71,7 @@ function App() {
         <Heading>Cotiza Criptomonedas al Instante</Heading>
         <Formulario setMonedas={setMonedas} />
       </div>  
+      {cargando && <Spinner />}
       {resultado.PRICE && <Resultado resultado={resultado} />}
     </Contenedor>
   );
