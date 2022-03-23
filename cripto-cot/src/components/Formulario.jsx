0f2a1320d43418d796793d505a03bbd82f507abd
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-22 21:12:28 -0300

    Validando Formulario

--- cripto-cot/src/components/Formulario.jsx
+++ cripto-cot/src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
+import Error from "./Error";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
 
@@ -22,6 +23,7 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
   const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
 
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
   const [criptomoneda, SelectCriptomoneda] = useSelectMonedas(
@@ -50,12 +52,26 @@ const Formulario = () => {
     consultarAPI();
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+  };
+
   return (
-    <form>
-      <SelectMonedas />
-      <SelectCriptomoneda />
-      <InputSubmit type="submit" value="Cotizar" />
-    </form>
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptomoneda />
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
   );
 };
 
