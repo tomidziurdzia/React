aa50b164bf124ae16792a96cb6705b2edb6cf0bb
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-25 18:45:38 -0300

    Usando useCallback y useRef para que no se modifique el resumen

--- cotizador-seguros/src/components/Resultado.jsx
+++ cotizador-seguros/src/components/Resultado.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import { useCallback, useMemo, useRef } from "react";
 import useCotizador from "../hooks/useCotizador";
 import { MARCAS, PLANES } from "../constants";
 
 const Resultado = () => {
   const { resultado, datos } = useCotizador();
   const { marca, plan, year } = datos;
+  const yearRef = useRef(year);
 
-  const [nombreMarca] = MARCAS.filter((m) => m.id === Number(marca));
-  const [nombrePlan] = PLANES.filter((p) => p.id === Number(plan));
+  const [nombreMarca] = useCallback(
+    MARCAS.filter((m) => m.id === Number(marca)),
+    [resultado]
+  );
+  const [nombrePlan] = useCallback(
+    PLANES.filter((p) => p.id === Number(plan)),
+    [resultado]
+  );
 
   if (resultado === 0) return null;
   return (
@@ -23,7 +30,7 @@ const Resultado = () => {
       </p>
       <p className="my-2">
         <span className="font-bold">Año: </span>
-        {year}
+        {yearRef.current}
       </p>
       <p className="my-2 text-2xl">
         <span className="font-bold">Total Cotizacion: </span>
