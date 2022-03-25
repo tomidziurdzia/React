d017a7d6de3935380e955fc983ceb90a7811c15a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-25 18:36:29 -0300

    Finalizando mostrar resumen

--- cotizador-seguros/src/components/Resultado.jsx
+++ cotizador-seguros/src/components/Resultado.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import useCotizador from "../hooks/useCotizador";
+import { MARCAS, PLANES } from "../constants";
 
 const Resultado = () => {
-  const { resultado } = useCotizador();
+  const { resultado, datos } = useCotizador();
+  const { marca, plan, year } = datos;
+
+  const [nombreMarca] = MARCAS.filter((m) => m.id === Number(marca));
+  const [nombrePlan] = PLANES.filter((p) => p.id === Number(plan));
 
   if (resultado === 0) return null;
-  return <div>{resultado}</div>;
+  return (
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
+      <p className="my-2">
+        <span className="font-bold">Marca: </span>
+        {nombreMarca.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Año: </span>
+        {year}
+      </p>
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotizacion: </span>
+        {resultado}
+      </p>
+    </div>
+  );
 };
 
 export default Resultado;
