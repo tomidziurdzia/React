1a755c32296b3b8161999fcc166c20ac4587c7be
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-29 18:50:33 -0300

    Creando la Edicion de Productos y Listado

--- redux-crud/src/components/Productos.js
+++ redux-crud/src/components/Productos.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Productos = () => {
-  return <h1>Desde productos</h1>;
+  return (
+    <Fragment>
+      <h2 className="text-center my-5">Listado de Productos</h2>
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th className="text-center" scope="col">
+              Nombre
+            </th>
+            <th className="text-center" scope="col">
+              Precio
+            </th>
+            <th className="text-center" scope="col">
+              Acciones
+            </th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default Productos;
