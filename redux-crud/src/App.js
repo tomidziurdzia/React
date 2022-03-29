2cef7f1700eec717d1aec2518b16d3a1c9489cae
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-29 18:16:08 -0300

    Creando el Header

--- redux-crud/src/App.js
+++ redux-crud/src/App.js
@@ -1,5 +1,11 @@
+import React, { Fragment } from "react";
+import Header from "./components/Header";
 function App() {
-  return <h1>Hola mundo</h1>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
