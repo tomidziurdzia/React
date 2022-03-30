217f05a5d2a9c15a812639c5f8023d8e3b15787f
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-30 19:17:57 -0300

    Mostrando errores

--- redux-crud/src/components/NuevoProducto.js
+++ redux-crud/src/components/NuevoProducto.js
@@ -12,6 +12,10 @@ const NuevoProducto = () => {
   // Utilizar useDispatch y crea una funcion
   const dispatch = useDispatch();
 
+  // Acceder al state del store
+  const cargando = useSelector((state) => state.productos.loading);
+  const error = useSelector((state) => state.productos.error);
+
   // Mandar a llamar el action de productoAction
   const agregarProducto = (producto) =>
     dispatch(crearNuevoProductoAction(producto));
@@ -72,6 +76,12 @@ const NuevoProducto = () => {
                 Agregar
               </button>
             </form>
+            {cargando ? <p>Cargando</p> : null}
+            {error ? (
+              <p className="alert alert-danger p-2 mt-4 text-center">
+                Hubo un error
+              </p>
+            ) : null}
           </div>
         </div>
       </div>
