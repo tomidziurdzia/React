554a41530c200620c2d14cedd8237b145e553d69
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-30 19:55:53 -0300

    Mostrando errores y texto de cargando

--- redux-crud/src/components/Productos.js
+++ redux-crud/src/components/Productos.js
@@ -18,10 +18,18 @@ const Productos = () => {
 
   // Obtener el state
   const productos = useSelector((state) => state.productos.productos);
+  const error = useSelector((state) => state.productos.error);
+  const cargando = useSelector((state) => state.productos.loading);
 
   return (
     <Fragment>
       <h2 className="text-center my-5">Listado de Productos</h2>
+      {error ? (
+        <p className="font-weigth-bold alert alerg-danger text-center mt-4">
+          Hubo un error
+        </p>
+      ) : null}
+      {cargando ? <p className="text-center">Cargando...</p> : null}
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
           <tr>
